c7f3f5d6269e393a6149f4daaa182c16974e455c
Sekou Samassi <samassi.s@northeastern.edu>
2024-07-31 19:38:38 -0500

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import ButtonAppBar from './components/AppBar.js';
+import * as React from 'react'
 
-function App() {
+export default function AppBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ButtonAppBar/>
     </div>
   );
-}
-
-export default App;
+  }
